6f63c73792a5edd39430ba2a30878ab5f5431e51
Evandro S. Motta <evandrosm@live.com>
2021-04-12 00:36:29 -0300

    Código que adiciona uma classe filha gerente a classe mãe funcionário, onde gerente herda os atributos e métodos da classe funcionário

---
+++ src/TeteGerente.java
@@ -0,0 +1,17 @@
+public class TeteGerente {
+    public static void main(String[] args) {
+        Gerente g1 = new Gerente();
+        g1.setNome("Evandro");
+        g1.setCpf("828.766.340-91");
+        g1.setSalario(5000.0);
+
+        System.out.println(g1.getNome());
+        System.out.println(g1.getCpf());
+        System.out.println(g1.getSalario());
+        g1.setSenha(1234);
+        boolean autenticou = g1.autentica(1234);
+        System.out.println(autenticou);
+
+
+    }
+}
---
+++ src/testeFuncionario.java
@@ -0,0 +1,14 @@
+public class testeFuncionario {
+    public static void main(String[] args) {
+        Funcionario evan = new Funcionario();
+        evan.setNome("Evan Motta");
+        evan.setCpf("838.766.349-91");
+        evan.setSalario(2732.22);
+
+        System.out.println("Nome: " + evan.getNome());
+        System.out.println("CPF: " + evan.getCpf());
+        System.out.println("Salário atual é de: R$" + evan.getSalario());
+        System.out.println("Novo salário é de: R$"
+                + (evan.getSalario() + evan.getBonificacao()));
+    }
+}
